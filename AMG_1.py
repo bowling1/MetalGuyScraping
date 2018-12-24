3e2d12ee05228e1e207e13259d4800199daaa2b3
bowling1 <42324148+bowling1@users.noreply.github.com>
2018-12-24 12:20:37 +0100

    Update AMG_1.py
    This pulls the titles of the Reviews from the Archives page

--- AMG_1.py
+++ AMG_1.py
@@ -1,44 +1,5 @@
 from requests import get
-from requests.exceptions import RequestException
-from contextlib import closing
 from bs4 import BeautifulSoup
-'''
-def simple_get(url):
-    """
-    Attempts to get the content at `url` by making an HTTP GET request.
-    If the content-type of response is some kind of HTML/XML, return the
-    text content, otherwise return None.
-    """
-    try:
-        with closing(get(url, stream=True)) as resp:
-            if is_good_response(resp):
-                return resp.content
-            else:
-                return None
-
-    except RequestException as e:
-        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
-        return None
-
-
-def is_good_response(resp):
-    """
-    Returns True if the response seems to be HTML, False otherwise.
-    """
-    content_type = resp.headers['Content-Type'].lower()
-    return (resp.status_code == 200 
-            and content_type is not None 
-            and content_type.find('html') > -1)
-
-
-def log_error(e):
-    """
-    It is always a good idea to log errors. 
-    This function just prints them, but you can
-    make it do anything.
-    """
-    print(e)
-'''
 
 url = 'https://www.angrymetalguy.com/category/reviews/page/100/'
 raw_html = get(url).text
@@ -47,7 +8,3 @@ html = BeautifulSoup(raw_html, 'html.parser')
 names = html.find_all(class_="entry-title")
 for i in range(1,len(names)):
 	print(names[i].get_text())
-
-
-#names = html.find(class_="entry-title").get_text()
-#print(names)
