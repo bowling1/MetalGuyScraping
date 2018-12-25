5281d858738fb6724acb2fa9446234ed4f0874d0
bowling1 <42324148+bowling1@users.noreply.github.com>
2018-12-25 17:47:26 +0100

    Update AMG_1.py
    Added comment, but what's there all together.

--- AMG_1.py
+++ AMG_1.py
@@ -9,22 +9,38 @@ def is_good_response(url):
 		return True
 	else:
 		return False
-pagenum = 406
+
+pagenum = 406 # Where the iteration starts. In the final test, 1.
 x=0
-name_list = []
+name_list = [] # List of links to the names of the albums (will be called "<ALBUM NAME> review")
+link_list = [] # List of links to reviews
 
 while x==0:
+	# Each time this loop iterates, it checks the next page of reviews
 	url = "https://www.angrymetalguy.com/category/reviews/page/" + str(pagenum) + "/"
+	# -------------------------------------------
+	# Checks to see if the website exists (should iterate 407 times)
 	if not is_good_response(url):
 		x = 1
-	pagenum +=1
+	# ------------------------------------------- 
+	# Creates the html of the website being iterated
 	raw_html = get(url).text
 	html = BeautifulSoup(raw_html, "html.parser")
 	names = html.find_all(class_="entry-title")
-	for i in range(1,len(names)):
-		name_list.append(names[i].get_text())
-
-print(name_list)
+	# -------------------------------------------
+	# Creates a list of names of the albums being reviewed
+	for i in range(1,len(names)):                         # There might be an error in starting at 1, maybe start at 0.
+		name_list.append(names[i].get_text())			  # Using 1 might excluding the first review on each page	
+	# -------------------------------------------
+	# Creates a list of links of the reviews on the page being iterated
+	link = names[0].find('a')
+	print(link.attrs['href'])
+	for i in range(0,len(names)):
+		link1 = names[i].find('a')
+		link2 = link1.attrs['href']
+		link_list.append(link2)
+	print(name_list)
+	pagenum +=1
 
 '''
 with open("Book1.csv", "a") as csvFile:
