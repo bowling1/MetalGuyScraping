19f95156021fcaba218aa89dd625f85b98cfa29a
bowling1 <42324148+bowling1@users.noreply.github.com>
2018-12-26 17:13:13 +0100

    Update AMG_1.py
    Now logs scores into list (with roughly estimated sleep time; should be optimized).

--- AMG_1.py
+++ AMG_1.py
@@ -1,6 +1,7 @@
 from requests import get
 from bs4 import BeautifulSoup
 import csv
+import time
 
 
 def is_good_response(url):
@@ -10,13 +11,23 @@ def is_good_response(url):
 	else:
 		return False
 
-pagenum = 406 # Where the iteration starts. In the final test, 1.
+def find_rating(url):
+	html_rating = BeautifulSoup(get(url).text,"html.parser")
+	html_string = str(html_rating)
+	for i in range(1,len(html_string)):
+		if html_string[i:i+4] == "/5.0":   # 1 review I've found has it as X/5 instead of X/5.0. Whatever.
+			return html_string[i-3:i]
+			break
+
+pagenum = 404 # Where the iteration starts. In the final test, 1.
 x=0
 name_list = [] # List of links to the names of the albums (will be called "<ALBUM NAME> review")
+rate_list = [] # List of ratings
 
 while x==0:
 	# Each time this loop iterates, it checks the next page of reviews
 	url = "https://www.angrymetalguy.com/category/reviews/page/" + str(pagenum) + "/"
+	pagenum +=1
 	# -------------------------------------------
 	# Checks to see if the website exists (should iterate 407 times)
 	if not is_good_response(url):
@@ -28,8 +39,8 @@ while x==0:
 	names = html.find_all(class_="entry-title")
 	# -------------------------------------------
 	# Creates a list of names of the albums being reviewed
-	for i in range(0,len(names)):                         # There might be an error in starting at 1, maybe start at 0.
-		name_list.append(names[i].get_text())			  # Using 1 might excluding the first review on each page	
+	for i in range(0,len(names)):                         
+		name_list.append(names[i].get_text())			  	
 	# -------------------------------------------
 	# Creates a list of links of the reviews on the page being iterated
 	link_list = [] # List of links to reviews. Refreshes every page loop.
@@ -37,11 +48,21 @@ while x==0:
 		link1 = names[i].find('a')
 		link2 = link1.attrs['href']
 		link_list.append(link2)
-	pagenum +=1
+	# -------------------------------------------
+	# Check the reviews for the score, and puts it in a list	
+	for i in range(0, len(link_list)-1):
+		time.sleep(1.5) #seconds
+		rate1 = find_rating(link_list[i])
+		if rate1 == None:
+			rate_list.append("No rating found")
+		else:
+			rate_list.append(rate1)
+
 '''
-with open("Book1.csv", "a") as csvFile:
-	writer = csv.writer(csvFile)
-	writer.writerow(name_list)
-csvFile.close()
-# This turns the list created into a row in a csv file
+# This turns the lists into a csv file
+out = csv.writer(open("Book1.csv","w"), delimiter=",", quoting=csv.QUOTE_ALL)
+out.writerow(name_list)
+out.writerow(rate_list)
 '''
+
+# Total runtime will be approximately 3 hours
